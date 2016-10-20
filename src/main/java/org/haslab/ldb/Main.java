b50d172e55de7044dd0dd586097a39d3fb6dba21
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-10-20 10:12:40 +0100

    Add simple test

--- src/main/java/org/haslab/ldb/Main.java
+++ src/main/java/org/haslab/ldb/Main.java
@@ -8,10 +8,16 @@ import org.haslab.ldb.exceptions.KeyAlreadyExistsException;
  * @author Vitor Enes (vitorenesduarte ~at~ gmail ~dot~ com)
  */
 public class Main {
+
     public static void main(String[] args) throws IOException, KeyAlreadyExistsException {
         LDB ldb = new LDB();
-        ldb.create("d", LDBType.GSet);
-        ldb.create("c", LDBType.GSet);
-        System.out.println("COOL");
+        ldb.create("a", LDBType.GSet);
+        ldb.create("b", LDBType.GSet);
+
+        try {
+            ldb.create("a", LDBType.GSet);
+        } catch (IOException | KeyAlreadyExistsException e) {
+            assert e instanceof KeyAlreadyExistsException;
+        }
     }
 }
